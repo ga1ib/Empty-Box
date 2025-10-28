ab7387efc602cab8f8009acbcbe7f84ed9a17e0d
ga1ib <galib52817636@gmail.com>
2025-10-28 18:47:56 +0600

    logo and name added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,7 +16,7 @@ const navbar = () => {
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
             <span className="text-xl tracking-tight">
-              VirtualR
+              Empty Box
             </span>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
